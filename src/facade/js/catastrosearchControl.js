77afb2b45f91f810528240165ee5efe9ac86a390
JGLeal <jgleal@gmail.com>
2019-02-18 11:59:33 +0100

    actualización plugins IDEA

--- src/facade/js/catastrosearchControl.js
+++ src/facade/js/catastrosearchControl.js
@@ -5,15 +5,15 @@ import CatastroSearchImplControl from 'impl/catastrosearchControl';
 export class CatastroSearchControl extends M.Control {
 
   /**
-   * @classdesc
-   * Main constructor of the class. Creates a PluginControl
-   * control
-   *
-   * @constructor
-   * @extends {M.Control}
-   * @api stable
-   */
-  constructor() {
+     * @classdesc
+     * Main constructor of the class. Creates a PluginControl
+     * control
+     *
+     * @constructor
+     * @extends {M.Control}
+     * @api stable
+     */
+  constructor () {
     // 1. checks if the implementation can create PluginControl
     if (M.utils.isUndefined(M.impl.control.CatastroSearchControl)) {
       M.exception('La implementación usada no puede crear controles CatastroSearchControl');
@@ -22,12 +22,16 @@ export class CatastroSearchControl extends M.Control {
     let impl = new M.impl.control.CatastroSearchControl();
     super(impl, "CatastroSearch");
 
+    // Inputs tabs
+    this.inputsTabs_ = null;
+    this.selectedTab_ = '';
     // Inputs y variables de buscador por polígono y parcela
     this.inputParcel_ = null;
     this.inputPolygon_ = null;
-    this.inputRC_ = null;
     this.selectMunicipality_ = null;
     this.selectProvince_ = null;
+    // Inputs por referencia
+    this.inputReference_ = null;
     // Inicio solo las provincias de Andalucia
     this.provincias_ = ["ALMERIA", "CADIZ", "CORDOBA", "GRANADA", "HUELVA", "JAEN", "MALAGA", "SEVILLA"];
   }
@@ -46,18 +50,41 @@ export class CatastroSearchControl extends M.Control {
     this.selectMunicipality_ = html.querySelector('.form div.municipality > select');
     this.inputParcel_ = html.querySelector('.form div.parcel > input');
     this.inputPolygon_ = html.querySelector('.form div.polygon > input');
-    this.inputRC_ = html.querySelector('.form div.refcatastral > input');
-
-
+    this.inputReference_ = html.querySelector('.form div.reference > input');
+    this.inputsTabs_ = html.querySelectorAll('input[name="tabs-catastro"]');
+    this.selectedTab_ = html.querySelector('input[name="tabs-catastro"]:checked').dataset.tab;
     // Asigno las funciones a cada evento
     this.clearBtn_.addEventListener('click', (evt) => this.clearInputs());
-    this.loadBtn_.addEventListener('click', (evt) => this.getRCFromPP());
+    this.loadBtn_.addEventListener('click', (evt) => this.getRC());
     this.selectProvince_.addEventListener('change', (evt) => this.changeProvince(evt));
     this.selectMunicipality_.addEventListener('change', (evt) => this.changeMunicipality(evt));
     this.inputParcel_.addEventListener('input', (evt) => this.checkButtons(evt));
     this.inputPolygon_.addEventListener('input', (evt) => this.checkButtons(evt));
-    this.inputRC_.addEventListener('input', (evt) => this.checkButtons(evt));
+    this.inputReference_.addEventListener('input', (evt) => this.checkButtons(evt));
 
+    // Annado el evento a cada radio button para saber cuando se ha cambiado de tab
+    this.inputsTabs_.forEach((element) => {
+      element.addEventListener('change', (evt) => this.changeTab(evt));
+    });
+  }
+
+  getRC() {
+    if (this.selectedTab_ == 'referencia') {
+      this.rc_ = this.inputReference_.value;
+      if (this.rc_.length > 14) {
+        this.rc_ = this.rc_.slice(0, 14);
+      }
+      this.getCoordinatesRC();
+    } else {
+      this.getRCFromPP();
+    }
+  }
+
+  changeTab(evt) {
+    evt = (evt || window.event);
+    let itemTarget = evt.target;
+    this.selectedTab_ = itemTarget.dataset.tab;
+    this.checkButtons();
   }
 
   /**
@@ -70,11 +97,7 @@ export class CatastroSearchControl extends M.Control {
    */
   createView(map) {
     return new Promise((success, fail) => {
-      M.template.compile('catastrosearch.html', {
-        vars: {
-          provincias: this.provincias_
-        }
-      }).then((html) => {
+      M.template.compile('catastrosearch.html', { vars: { provincias: this.provincias_ } }).then((html) => {
         //Establecer eventos
         this.addEvents(html);
         success(html);
@@ -87,21 +110,17 @@ export class CatastroSearchControl extends M.Control {
    *
    * @memberof CatastroSearchControl
    */
-  checkButtons(evt) {
-    this.clearBtn_.disabled = (M.utils.isNullOrEmpty(this.selectProvince_.value) && M.utils.isNullOrEmpty(this.selectMunicipality_.value) &&
-      M.utils.isNullOrEmpty(this.inputPolygon_.value) && M.utils.isNullOrEmpty(this.inputParcel_.value) && M.utils.isNullOrEmpty(this.inputRC_.value));
-    this.loadBtn_.disabled = ((M.utils.isNullOrEmpty(this.selectProvince_.value) || M.utils.isNullOrEmpty(this.selectMunicipality_.value) ||
-      M.utils.isNullOrEmpty(this.inputPolygon_.value) || M.utils.isNullOrEmpty(this.inputParcel_.value)) && M.utils.isNullOrEmpty(this.inputRC_.value));
-
-    if (evt) {
-      if (evt.target === this.inputRC_) {
-        this.selectProvince_.value = this.selectMunicipality_.value =
-          this.inputPolygon_.value =
-          this.inputParcel_.value = "";
-      } else { //if (evt.target === this.inputParcel_ || evt.target === this.inputPolygon_)
-        this.inputRC_.value = '';
-      }
+  checkButtons() {
+    if (this.selectedTab_ == 'referencia') {
+      this.clearBtn_.disabled = (M.utils.isNullOrEmpty(this.inputReference_.value));
+      this.loadBtn_.disabled = (M.utils.isNullOrEmpty(this.inputReference_.value));
+    } else {
+      this.clearBtn_.disabled = (M.utils.isNullOrEmpty(this.selectProvince_.value) && M.utils.isNullOrEmpty(this.selectMunicipality_.value)
+        && M.utils.isNullOrEmpty(this.inputPolygon_.value) && M.utils.isNullOrEmpty(this.inputParcel_.value));
+      this.loadBtn_.disabled = (M.utils.isNullOrEmpty(this.selectProvince_.value) || M.utils.isNullOrEmpty(this.selectMunicipality_.value)
+        || M.utils.isNullOrEmpty(this.inputPolygon_.value) || M.utils.isNullOrEmpty(this.inputParcel_.value));
     }
+
   }
   /**
    * Elimina los valores de los inputs y limpio la capa de features
@@ -109,24 +128,26 @@ export class CatastroSearchControl extends M.Control {
    * @memberof CatastroSearchControl
    */
   clearInputs() {
-
-    this.inputPolygon_.value = '';
-    this.inputParcel_.value = '';
-    this.selectProvince_.value = '';
-    this.inputRC_.value = '';
-    this.selectMunicipality_.value = '';
+    if (this.selectedTab_ == 'referencia') {
+      this.inputReference_.value = '';
+    } else {
+      this.inputPolygon_.value = '';
+      this.inputParcel_.value = '';
+      this.selectProvince_.value = '';
+      this.selectMunicipality_.value = '';
+    }
     this.checkButtons();
     //Limpia la capa de coordenadas
     this.getImpl().clearCoordinatesLayer();
   }
 
   changeProvince(evt) {
-    this.checkButtons(evt);
+    this.checkButtons();
     this.getMunicipios();
   }
 
   changeMunicipality(evt) {
-    this.checkButtons(evt);
+    this.checkButtons();
   }
 
   /**
@@ -135,13 +156,11 @@ export class CatastroSearchControl extends M.Control {
    * @memberof CatastroSearchControl
    */
   getMunicipios() {
+    if (this.selectedTab_ == 'referencia') {
+      this.rc_ = this.inputReference_.value;
+    }
     let municipiosURL = "http://ovc.catastro.meh.es/ovcservweb/OVCSWLocalizacionRC/OVCCallejero.asmx/ConsultaMunicipio";
-    M.remote.get(municipiosURL, {
-      Provincia: this.selectProvince_.value,
-      Municipio: ''
-    }, {
-      'jsonp': true
-    }).then((response) => {
+    M.remote.get(municipiosURL, { Provincia: this.selectProvince_.value, Municipio: '' }, { 'jsonp': true }).then((response) => {
       // Parseamos el XML devuelto
       let parser = new DOMParser();
       let xmlDoc = parser.parseFromString(response.text, "text/xml");
@@ -150,9 +169,7 @@ export class CatastroSearchControl extends M.Control {
       //JGL: el length = 0 puede dar problemas en algunos navegadores
       this.selectMunicipality_.innerHTML = null;
       this.createMunicipalityOption_("Municipio...", "");
-      if (response.error) {
-        M.dialog.error("Error al consultar al catastro: inténtelo de nuevo pasados unos segundos");
-      } else if (error.length) {
+      if (error.length) {
         M.dialog.error("Error al consultar al catastro: " + error[0].childNodes[0].nodeValue);
       } else {
         let municipios = xmlDoc.getElementsByTagName("nm");
@@ -183,16 +200,11 @@ export class CatastroSearchControl extends M.Control {
    *
    * @memberof CatastroSearchControl
    */
-  getCoordinatesRC(refCatastral) {
+  getCoordinatesRC() {
     let coordinatesURL = "http://ovc.catastro.meh.es/ovcservweb/OVCSWLocalizacionRC/OVCCoordenadas.asmx/Consulta_CPMRC";
     M.remote.get(coordinatesURL, {
-      Provincia: '',
-      Municipio: '',
-      SRS: 'EPSG:4326',
-      RC: refCatastral
-    }, {
-      'jsonp': true
-    }).then((response) => {
+      Provincia: '', Municipio: '', SRS: 'EPSG:4326', RC: this.rc_
+    }, { 'jsonp': true }).then((response) => {
       // Parseamos el XML devuelto
       let parser = new DOMParser();
       let xmlDoc = parser.parseFromString(response.text, "text/xml");
@@ -217,35 +229,24 @@ export class CatastroSearchControl extends M.Control {
    * @memberof CatastroSearchControl
    */
   getRCFromPP() {
-    if (M.utils.isNullOrEmpty(this.inputRC_.value)) {
-      let rcURL = "http://ovc.catastro.meh.es/ovcservweb/OVCSWLocalizacionRC/OVCCallejero.asmx/Consulta_DNPPP";
-      M.remote.get(rcURL, {
-        Provincia: this.selectProvince_.value,
-        Municipio: this.selectMunicipality_.value,
-        Poligono: this.inputPolygon_.value,
-        Parcela: this.inputParcel_.value
-      }, {
-        'jsonp': true
-      }).then((response) => {
-        // Parseamos el XML devuelto
-        let parser = new DOMParser();
-        let xmlDoc = parser.parseFromString(response.text, "text/xml");
-        let error = xmlDoc.getElementsByTagName("des");
-        if (error.length) {
-          M.dialog.info(error[0].childNodes[0].nodeValue);
-        } else {
-          // Construyo el nombre del registro catastral
-          let pc1 = xmlDoc.getElementsByTagName("pc1");
-          let pc2 = xmlDoc.getElementsByTagName("pc2");
-          let rc = pc1[0].childNodes[0].nodeValue + pc2[0].childNodes[0].nodeValue;
-          this.inputRC_.value = rc;
-          this.getCoordinatesRC(this.inputRC_.value);
-        }
-      });
-    } else {
-      this.getCoordinatesRC(this.inputRC_.value);
-    }
+    let rcURL = "http://ovc.catastro.meh.es/ovcservweb/OVCSWLocalizacionRC/OVCCallejero.asmx/Consulta_DNPPP";
+    M.remote.get(rcURL, {
+      Provincia: this.selectProvince_.value, Municipio: this.selectMunicipality_.value, Poligono: this.inputPolygon_.value, Parcela: this.inputParcel_.value
+    }, { 'jsonp': true }).then((response) => {
+      // Parseamos el XML devuelto
+      let parser = new DOMParser();
+      let xmlDoc = parser.parseFromString(response.text, "text/xml");
+      let error = xmlDoc.getElementsByTagName("des");
+      if (error.length) {
+        M.dialog.info(error[0].childNodes[0].nodeValue);
+      } else {
+        // Construyo el nombre del registro catastral
+        let pc1 = xmlDoc.getElementsByTagName("pc1");
+        let pc2 = xmlDoc.getElementsByTagName("pc2");
+        this.rc_ = pc1[0].childNodes[0].nodeValue + pc2[0].childNodes[0].nodeValue;
+        this.getCoordinatesRC();
+      }
+    });
   }
-
 
 }
